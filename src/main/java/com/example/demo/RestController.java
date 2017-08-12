610f9a58aa71cc8a8ad9ae81bbc6c4722715a7ec
naveedakhtar019 <30958386+naveedakhtar019@users.noreply.github.com>
2017-08-12 22:37:34 +0530

    Spring boot rest api starter

---
+++ src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api")
+public class RestController {
+  
+  @RequestMapping(value = "/message", method = RequestMethod.GET)
+  public ResponseEntity<String> message() {
+     
+      return new ResponseEntity<String>("hello", HttpStatus.OK);
+  }
+
+}
